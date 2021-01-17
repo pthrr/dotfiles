168998e3fb4afdc9ee15dd4d94e153496d046337
pthrr <pthrr@posteo.de>
2021-01-17 13:10:52 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install curl wget git neovim
+sudo apt-get install xmonad xterm suckless-tools xpmroot x11-xserver-utils xmobar
+sudo apt-get install xcircuit xlog xfig xpdf x11-apps
+sudo apt-get install tmux
