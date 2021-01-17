e07432dd006fe7b516ab4294c75ca8426f481d3d
pthrr <pthrr@posteo.de>
2021-01-17 15:59:47 +0100

    status bar + small fixes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,8 +11,12 @@ xmonad $ def
   { terminal = "xterm"
   , manageHook = manageDocks <+> manageHook def
   , layoutHook = avoidStruts $ layoutHook def
-  , logHook = dynamicLogWithPP $ def { ppOutput = hPutStrLn xmproc, ppOrder = \(ws:_:t:_) -> [ws,t] }
+  , handleEventHook = handleEventHook def <+> docksEventHook
+  , logHook = dynamicLogWithPP $ def
+  	{ ppOutput = hPutStrLn xmproc
+	, ppOrder = \(ws:_:t:_) -> [ws,t]
+	}
   , borderWidth = 3
   }
   `additionalKeys`
-  [ ((mod1Mask, xK_p), spawn "exe=`dmenu_path | /home/geohot/.cabal/bin/yeganesh -- -b -fn xft:Inconsolata:size=16` && eval \"exec $exe\"") ]
+  [ ((mod1Mask, xK_p), spawn "exe=`dmenu_path | /home/pthrr/.cabal/bin/yeganesh -- -b -fn xft:Inconsolata:size=16` && eval \"exec $exe\"") ]
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install curl wget git neovim
-sudo apt-get install xmonad xmobar xterm suckless-tools xpmroot x11-xserver-utils xmobar
+sudo apt-get install xmonad xmobar xterm suckless-tools x11-xserver-utils
 sudo apt-get install xcircuit xlog xfig xpdf x11-apps
 sudo apt-get install tmux cabal-install
 cabal update
