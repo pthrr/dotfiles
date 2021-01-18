a376f06efcb4fca19b7f8daafb1d1df575ece80a
pthrr <pthrr@posteo.de>
2021-01-18 15:59:27 +0100

    small fixes for use

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt-get install curl wget git neovim build-essential gcc-8 cabal-install
 sudo apt-get install xmonad xmobar xterm suckless-tools x11-xserver-utils
-sudo apt-get install xcircuit xlog xfig xpdf x11-apps
+sudo apt-get install xcircuit xlog xfig xpdf xbacklight x11-apps
 sudo apt-get install tmux ssh-askpass
-sudo apt-get install pulseaudio-utils pavucontrol xbacklight
+sudo apt-get install pulseaudio-utils pavucontrol
 sudo apt-get install stalonetray network-manager-gnome redshift
 cabal update
 cabal install yeganesh
