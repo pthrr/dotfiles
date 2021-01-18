6eb608cc780905bcac8c4c2f76b3d1e39e30542a
pthrr <pthrr@posteo.de>
2021-01-18 14:12:59 +0100

    added media keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Util.Run
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.CustomKeys
 import XMonad.Util.EZConfig
+import Graphics.X11.ExtraTypes.XF86
 
 main = do
 xmproc <- spawnPipe "xmobar"
@@ -19,4 +20,11 @@ xmonad $ def
   , borderWidth = 3
   }
   `additionalKeys`
-  [ ((mod1Mask, xK_p), spawn "exe=`dmenu_path | /home/pthrr/.cabal/bin/yeganesh -- -b -fn xft:Inconsolata:size=12` && eval \"exec $exe\"") ]
+  [ ((mod1Mask, xK_p), spawn "exe=`dmenu_path | /home/pthrr/.cabal/bin/yeganesh -- -b -fn xft:Inconsolata:size=12` && eval \"exec $exe\"")
+  , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +10")
+  , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -10")
+  , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+  , ((0, xF86XK_AudioMicMute), spawn "pactl set-source-mute @DEFAULT_SOURCE@ toggle")
+  , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -10%")
+  , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +10%")
+  ]
