d8579aeb1d641430eb66677c4e53488aff958e1f
pthrr <pthrr@posteo.de>
2021-01-22 06:44:38 +0100

    added chromium

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-sudo apt-get install curl wget git neovim build-essential gcc-8 cabal-install
+sudo apt-get install curl wget git neovim chromium build-essential gcc-8
 sudo apt-get install libx11-dev libxinerama-dev libxext-dev
 sudo apt-get install libxrandr-dev libxss-dev libxft-dev
 sudo apt-get install xterm suckless-tools x11-xserver-utils
 sudo apt-get install xcircuit xlog xfig xpdf xbacklight x11-apps
-sudo apt-get install tmux ssh-askpass
-sudo apt-get install pulseaudio-utils pavucontrol
-sudo apt-get install stalonetray network-manager-gnome redshift
+sudo apt-get install tmux ssh-askpass network-manager-gnome
+sudo apt-get install stalonetray pulseaudio-utils pavucontrol redshift
+sudo apt-get install cabal-install
 cabal update
 cabal install xmonad xmonad-contrib xmobar yeganesh
