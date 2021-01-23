23356c0213b949970721ec803462c0a23a0897cf
pthrr <pthrr@posteo.de>
2021-01-23 17:16:37 +0100

    more stuff

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get install build-essential cmake automake g++-10 gcc-10
 sudo apt-get install libx11-dev libxinerama-dev libxext-dev
 sudo apt-get install libxrandr-dev libxss-dev libxft-dev
 sudo apt-get install xterm suckless-tools x11-xserver-utils
-sudo apt-get install xcircuit xlog xfig xpdf xbacklight x11-apps
+sudo apt-get install xarchiver xcircuit xlog xfig xpdf xbacklight x11-apps
 sudo apt-get install tmux ssh-askpass network-manager-gnome
 sudo apt-get install stalonetray pulseaudio-utils pavucontrol redshift
 sudo apt-get install cabal-install && cabal update
