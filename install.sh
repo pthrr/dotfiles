cfc02872a6f49fb61254369c2e2516e8f25196eb
pthrr <pthrr@posteo.de>
2021-01-24 14:35:17 +0100

    fix for ubuntu 20.04

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-sudo apt-get install curl wget git gitg neovim chromium
-sudo apt-get install build-essential cmake automake g++-10 gcc-10
-sudo apt-get install libx11-dev libxinerama-dev libxext-dev
-sudo apt-get install libxrandr-dev libxss-dev libxft-dev
-sudo apt-get install xterm suckless-tools x11-xserver-utils
-sudo apt-get install xarchiver xcircuit xlog xfig xpdf xbacklight x11-apps
-sudo apt-get install tmux ssh-askpass network-manager-gnome
-sudo apt-get install stalonetray pulseaudio-utils pavucontrol redshift
-sudo apt-get install cabal-install && cabal update
-cabal install xmonad xmonad-contrib xmobar yeganesh
+sudo apt-get install -y perl curl wget git gitg neovim chromium-browser
+sudo apt-get install -y build-essential cmake automake g++-10 gcc-10
+sudo apt-get install -y libx11-dev libxinerama-dev libxext-dev
+sudo apt-get install -y libxrandr-dev libxss-dev libxft-dev
+sudo apt-get install -y xterm suckless-tools x11-xserver-utils
+sudo apt-get install -y xarchiver xcircuit xlog xfig xbacklight x11-apps
+sudo apt-get install -y tmux ssh-askpass network-manager-gnome
+sudo apt-get install -y stalonetray pulseaudio-utils pavucontrol redshift
+sudo apt-get install -y cabal-install && cabal v1-update
+cabal v1-install xmonad xmonad-contrib xmobar yeganesh
