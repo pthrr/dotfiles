16c1ec9d7811b1b303ee1fb4bb6e25bd19fddb4d
pthrr <pthrr@posteo.de>
2021-01-24 22:33:23 +0100

    sudo fix

--- install-xpdf.sh
+++ install-xpdf.sh
@@ -1,6 +1,6 @@
 sudo mkdir ~/bin ~/opt ~/tmp
 cd ~/tmp
-wget http://security.ubuntu.com/ubuntu/pool/main/p/poppler/libpoppler73_0.62.0-2ubuntu2.12_amd64.deb
+sudo wget http://security.ubuntu.com/ubuntu/pool/main/p/poppler/libpoppler73_0.62.0-2ubuntu2.12_amd64.deb
 sudo apt-get install -y ./libpoppler73_0.62.0-2ubuntu2.12_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/universe/x/xpdf/xpdf_3.04-7_amd64.deb
+sudo wget http://archive.ubuntu.com/ubuntu/pool/universe/x/xpdf/xpdf_3.04-7_amd64.deb
 sudo apt-get install -y ./xpdf_3.04-7_amd64.deb
