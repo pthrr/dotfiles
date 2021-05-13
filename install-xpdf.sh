858bc2535156815e2dd4c3b43f4a75d027f3fe52
pthrr <pthrr@posteo.de>
2021-05-13 19:45:44 +0200

    switched to stow

--- install-xpdf.sh
+++
@@ -1,6 +0,0 @@
-mkdir ~/tmp
-cd ~/tmp
-sudo wget http://security.ubuntu.com/ubuntu/pool/main/p/poppler/libpoppler73_0.62.0-2ubuntu2.12_amd64.deb
-sudo apt-get install -y ./libpoppler73_0.62.0-2ubuntu2.12_amd64.deb
-sudo wget http://archive.ubuntu.com/ubuntu/pool/universe/x/xpdf/xpdf_3.04-7_amd64.deb
-sudo apt-get install -y ./xpdf_3.04-7_amd64.deb
--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
-sudo apt-get install -y perl curl wget git gitg neovim chromium-browser
+sudo apt-get install -y perl curl wget git gitg neovim chromium-browser nautilus
 sudo apt-get install -y build-essential cmake automake g++-10 gcc-10
 sudo apt-get install -y libx11-dev libxinerama-dev libxext-dev
 sudo apt-get install -y libxrandr-dev libxss-dev libxft-dev
 sudo apt-get install -y xterm suckless-tools x11-xserver-utils
-sudo apt-get install -y xarchiver xcircuit xlog xfig xbacklight xfe x11-apps
+sudo apt-get install -y xarchiver xpdf xcircuit xlog xfig xbacklight xfe x11-apps
 sudo apt-get install -y tmux ssh-askpass network-manager-gnome
 sudo apt-get install -y stalonetray pulseaudio-utils pavucontrol redshift
-sudo apt-get install -y cabal-install && cabal v1-update
-cabal v1-install xmonad xmonad-contrib xmobar yeganesh
-printf "[Desktop Entry]\nName=Xmonad\nComment=Tiling WM\nExec=xmonad\nType=XSession" | sudo tee /usr/share/xsessions/Xmonad.desktop
+sudo apt-get install -y cabal-install && cabal update
+cabal install xmonad xmonad-contrib xmobar yeganesh
 curl https://sh.rustup.rs -sSf | sh
