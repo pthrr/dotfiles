5087ccbe1e91dc4bbef66d5b2e8bc989e5f2416d
pthrr <pthrr@posteo.de>
2022-01-23 16:53:49 +0100

    Clean up
    Why:
    
    How:
    
    Tags:

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -397,18 +397,6 @@
               }
             EOF
             nmap <F5> :TodoQuickFix cwd=.<CR>
-            " fzf
-            "set grepprg=rg\ --vimgrep\ --smart-case\ --follow
-            "nnoremap <silent> <C-f> :Files<CR>
-            "nnoremap <silent> <Leader>f :Ag<CR>
-            "nnoremap <silent> <Leader>b :Buffers<CR>
-            "nnoremap <silent> <Leader>/ :BLines<CR>
-            "nnoremap <silent> <Leader>' :Marks<CR>
-            "nnoremap <silent> <Leader>g :Commits<CR>
-            "nnoremap <silent> <Leader>H :Helptags<CR>
-            "nnoremap <silent> <Leader>hh :History<CR>
-            "nnoremap <silent> <Leader>h: :History:<CR>
-            "nnoremap <silent> <Leader>h/ :History/<CR> 
             " ultisnips
             let g:UltiSnipsExpandTrigger = '<tab>'
             let g:UltiSnipsJumpForwardTrigger = '<tab>'
@@ -504,7 +492,6 @@
             vim-gutentags
             ultisnips
             vimagit
-            #fzf-vim
             todo-comments-nvim
           ];
         };
