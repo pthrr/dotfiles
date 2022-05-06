51bc4f55586d46701f6c036298d11c98581c5258
pthrr <pthrr@posteo.de>
2022-05-06 14:55:33 +0200

    Some fixes
    What:
    
    Why:
    
    How:
    
    Tags:

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,17 +4,13 @@ cd "$(dirname "$0")"
 
 # Bug: Stow cant properly use --dotfiles option on folders right now
 
-# install only first argument
-if [ -d "${1+null}" ] ; then
-    echo "Stowing $1 .."
-    stow --no-folding --dotfiles --target="$HOME" "$1"
-fi
-
-# giving no argument will install all in current folder
-if [ -z "${1+null}" ] ; then
+if [ -z "${1+n}" ] ; then
     for d in */ ; do
         [ -L "${d%/}" ] && continue
         echo "Stowing $d .."
         stow --no-folding --dotfiles --target="$HOME" "$d"
     done
+else
+    echo "Stowing $1 .."
+    stow --no-folding --dotfiles --target="$HOME" "$1"
 fi
