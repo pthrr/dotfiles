84c9a4b555c44aa27348f85b2da7a18b8c4c125a
pthrr <pthrr@posteo.de>
2022-05-28 16:30:16 +0200

    More
    What:
    
    Why:
    
    How:
    
    Tags:

--- system/install.sh
+++ system/install.sh
@@ -8,19 +8,22 @@ sudo apt-get install -y \
     python3 \
     python3-pip \
     python3-venv \
+    python3-mypy \
     pulseaudio \
     pulseaudio-utils \
     xorg \
     x11-apps \
     x11-xserver-utils \
     software-properties-common \
-    firmware-misc-nonfree \
-    intel-microcode \
+    #firmware-misc-nonfree \
+    #intel-microcode \
     #laptop-mode-tools \
     #xserver-xorg-input-synaptics \
     #lm-sensors \
-    adwaita-icon-theme \
-    adwaita-qt \
-    qt5ct \
-    mtp-tools \
-    xcircuit
+    #adwaita-icon-theme \
+    #adwaita-qt \
+    #qt5ct \
+    #mtp-tools \
+    #xcircuit
+source ~/.profile
+pip3 install -r requirements.txt
