195db84ac5f53a992b5b006a6f5611caaa6baca7
pthrr <pthrr@posteo.de>
2022-08-09 05:02:31 +0200

    Fixes
    What:
    
    Why:
    
    How:
    
    Tags:

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
+set -euo pipefail
 cd "$(dirname "$0")"
 
 # Bug: Stow cant properly use --dotfiles option on folders right now
--- system/install.sh
+++ system/install.sh
@@ -3,30 +3,16 @@ set -euo pipefail
 
 sudo apt-get update
 sudo apt-get install -y \
-    #libtool \
     build-essential \
     automake \
     autoconf \
-    #perl \
     python3 \
     python3-pip \
-    #python3-venv \
-    #python3-mypy \
-    #pulseaudio \
-    #pulseaudio-utils \
-    #xorg \
-    #x11-apps \
-    #x11-xserver-utils \
-    #software-properties-common \
-    #firmware-misc-nonfree \
-    #intel-microcode \
-    #laptop-mode-tools \
-    #xserver-xorg-input-synaptics \
-    #lm-sensors \
-    #adwaita-icon-theme \
-    #adwaita-qt \
-    #qt5ct \
-    #mtp-tools \
-    #xcircuit
-#source ~/.profile
+    firmware-misc-nonfree \
+    intel-microcode \
+    adwaita-icon-theme \
+    adwaita-qt \
+    qt5ct \
+
 python3 -m pip install i3ipc
+python3 -m pip install mypy
