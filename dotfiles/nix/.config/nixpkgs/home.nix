3dcf7696165c67be88a7f179635e19e2386d3b11
pthrr <pthrr@posteo.de>
2022-09-17 20:30:11 +0200

    Added drawio
    What:
    
    Why:
    
    How:
    
    Tags:

--- dotfiles/nix/.config/nixpkgs/home.nix
+++ dotfiles/nix/.config/nixpkgs/home.nix
@@ -19,6 +19,7 @@
         enableNixpkgsReleaseCheck = true;
 
         packages = [
+            pkgs.drawio
             pkgs.lmms
             pkgs.texlive.combined.scheme-basic
             pkgs.cling
