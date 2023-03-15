e0c3f773fa98bda35a8bd038acd28ef632f10df8
pthrr <pthrr@posteo.de>
2023-03-16 00:03:47 +0100

    Fix
    What:
    
    Why:
    
    How:
    
    Tags:

--- dotfiles/wezterm/.config/wezterm/wezterm.lua
+++ dotfiles/wezterm/.config/wezterm/wezterm.lua
@@ -1,21 +1,16 @@
 local wezterm = require 'wezterm'
 return {
-    font = wezterm.font 'Ubuntu Mono',
-    font_size = 11,
-    color_scheme = "Solarized Dark (base16)",
     check_for_updates = false,
-    animation_fps = 1,
+    font = wezterm.font 'Ubuntu Mono',
+    font_size = 12,
+    color_scheme = "Solarized Dark (base16)",
     cursor_blink_rate = 0,
-    enable_wayland = false,
-    hide_tab_bar_if_only_one_tab = false,
+    use_fancy_tab_bar = false,
+    tab_bar_at_bottom = true,
     window_padding = {
         left = 5,
         right = 5,
         top = 5,
         bottom = 0,
     },
-    use_fancy_tab_bar = false,
-    exit_behavior = "CloseOnCleanExit",
-    tab_bar_at_bottom = true,
-    window_close_confirmation = "AlwaysPrompt",
 }
