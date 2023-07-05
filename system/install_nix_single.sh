c5daa40cae5730f5dbc649c86eafe723688b9f87
pthrr <pthrr@posteo.de>
2023-07-06 01:04:22 +0200

    Fix
    What:
    
    Why:
    
    How:
    
    Tags:

--- system/install_nix_single.sh
+++ system/install_nix_single.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 sudo install -d -m755 -o $(id -u) -g $(id -g) /nix
 curl -L https://nixos.org/nix/install | sh
-nix-env -i hello
 . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+nix-env -i hello
 which hello
 hello
