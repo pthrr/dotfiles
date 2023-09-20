0ac524d12443c2b9c66f1aff47324656a04841c6
pthrr <pthrr@posteo.de>
2023-09-20 10:59:00 +0200

    Added kbrdd
    What:
    
    Why:
    
    How:
    
    Tags:

--- dotfiles/misc/kbdbacklight/src/main.zig
+++ dotfiles/misc/kbdbacklight/src/main.zig
@@ -1,21 +1,22 @@
 const std = @import("std");
-const os = std.os;
 const fs = std.fs;
+const os = std.os;
+const linux = os.linux;
+
 const POLLIN = 0x0001;
+const TIMEOUT_IDLE_MS: i32 = 5000;
+const BRIGHTNESS = enum(u8) {
+    OFF = 0,
+    MED = 1,
+    MAX = 2,
+};
 
 pub fn main() !void {
-    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(general_purpose_allocator.deinit() == .ok);
-    const gpa = general_purpose_allocator.allocator();
-    _ = gpa;
-
-    // var running: bool = true;
     // fn term() void {
     //     running= false;
     // }
-    const IDLE_MSEC: u64 = 5000;
-    const BRGHT_OFF: u8 = '0';
-    const BRGHT_MED: u8 = '1';
+    // const IDLE_MSEC: u64 = 5000;
+    // var timeout: i32 = @as(i32, @intCast(IDLE_MSEC));
 
     // // Set up signal handling for SIGTERM
     // const term_action = os.Sigaction{
@@ -25,34 +26,36 @@ pub fn main() !void {
     // };
     // try os.sigaction(os.SIGTERM, &term_action, null);
 
-    // Open the backlight device file
-    // const blfd = try fs.cwd().openFile("/sys/class/leds/tpacpi::kbd_backlight/brightness", fs.File.OpenFlags{ .mode = fs.File.OpenMode.write_only });
+    const blfd = try fs.cwd().openFile("/sys/class/leds/tpacpi::kbd_backlight/brightness", fs.File.OpenFlags{ .mode = fs.File.OpenMode.write_only });
+    defer blfd.close();
 
-    // Open the input device file
-    const pfd = try fs.cwd().openFile("/dev/input/event3", fs.File.OpenFlags{ .mode = fs.File.OpenMode.read_only });
+    const pfd = try fs.cwd().openFile("/dev/input/event0", fs.File.OpenFlags{ .mode = fs.File.OpenMode.read_only });
+    defer pfd.close();
 
-    var timeout: i32 = @as(i32, @intCast(IDLE_MSEC));
-    var prev: i32 = -1;
-    var bm: u8 = BRGHT_MED;
-    var dummybuf: [8192]u8 = undefined;
-    var poll_fd = os.linux.pollfd{ .fd = pfd.handle, .events = POLLIN, .revents = undefined };
+    var poll_fd = linux.pollfd{ .fd = pfd.handle, .events = POLLIN, .revents = undefined };
+    var poll_fds = [_]linux.pollfd{poll_fd};
+
+    var prev: ?BRIGHTNESS = null;
+    var bm: BRIGHTNESS = BRIGHTNESS.MED;
+    var buf: [8192]u8 = undefined;
     var rc: u64 = 0;
+    var timeout: i32 = -1;
+    var exit_requested: bool = false;
 
-    while (true) {
-        var array = [_]os.linux.pollfd{poll_fd};
-        rc = os.linux.poll(array[0..], 1, timeout);
+    while (!exit_requested) {
+        rc = linux.poll(poll_fds[0..], 1, timeout);
 
         if (rc > 0) {
-            // Drain all pending events from the input device
-            _ = try pfd.read(dummybuf[0..]);
-            timeout = @as(i32, @intCast(IDLE_MSEC));
-            bm = BRGHT_MED;
+            std.debug.print("key was pressed", .{});
+            _ = try pfd.read(buf[0..]); // Drain all pending events from the input device
+            timeout = TIMEOUT_IDLE_MS;
+            bm = BRIGHTNESS.MAX;
         } else if (rc == 0) {
-            // No events within timeout, turn off backlight
+            std.debug.print("idling", .{});
             timeout = -1;
-            bm = BRGHT_OFF;
+            bm = BRIGHTNESS.OFF;
         } else {
-            std.debug.print("Poll error", .{}); //: {}\n", .{os.strerror(rc)});
+            std.debug.print("poll error", .{});
             continue;
         }
 
@@ -60,14 +63,27 @@ pub fn main() !void {
             continue;
         }
 
-        // Seek and write brightness level
-        // try blfd.seekTo(13);
         std.debug.print("writing brightness", .{});
-        // try blfd.write(&[1]u8{bm});
-        // prev = bm;
+        _ = try blfd.seekTo(13);
+        // switch (seek_result) {
+        //     @TypeOf(void) => {},
+        //     else => {
+        //         std.debug.print("Failed to seek: {}\n", .{seek_result});
+        //         return null;
+        //     },
+        // }
+        _ = try blfd.write(&[_]u8{@intFromEnum(bm)});
+        // switch (write_result) {
+        //     usize => {},
+        //     else => {
+        //         std.debug.print("Failed to write: {}\n", .{write_result});
+        //         return null;
+        //     },
+        // }
+        prev = bm;
     }
 
-    // Clean-up, set brightness to off
-    // try blfd.seekTo(13);
-    // try blfd.write(&[1]u8{BRGHT_OFF});
+    std.debug.print("clean up", .{});
+    _ = try blfd.seekTo(13);
+    _ = try blfd.write(&[1]u8{@intFromEnum(BRIGHTNESS.OFF)});
 }
