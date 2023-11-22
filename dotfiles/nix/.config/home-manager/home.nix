4af4642ec250f6235ef3c9863bc48545622358a4
pthrr <pthrr@posteo.de>
2023-11-22 23:43:24 +0100

    Fix
    What:
    
    Why:
    
    How:
    
    Tags:

--- dotfiles/nix/.config/home-manager/home.nix
+++ dotfiles/nix/.config/home-manager/home.nix
@@ -25,7 +25,8 @@
             jetbrains.pycharm-professional micromamba
             jetbrains.jdk jetbrains.idea-ultimate
             kotlin kotlin-native kotlin-language-server detekt ktlint
-            typescript nodejs_20 nodePackages_latest.eslint deno bun esbuild
+            typescript esbuild nodePackages_latest.eslint nodePackages_latest.nodemon
+            deno bun nodejs_20
             nodePackages_latest.fixjson
             typst typst-fmt typst-lsp
             scala_3 sbt-with-scala-native scalafmt scalafix scala-cli metals
