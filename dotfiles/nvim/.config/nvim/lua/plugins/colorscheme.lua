bf4a84095ce592757a373b382f635007336fb29b
pthrr <pthrr@posteo.de>
2024-10-04 02:00:08 +0200

    Cleanup
    What:
    
    Why:
    
    How:
    
    Tags:

--- dotfiles/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "overcache/NeoSolarized",
-    priority = 1000,
-    config = function()
-      vim.opt.termguicolors = true
-      vim.opt.background = "dark"
-      vim.cmd.colorscheme 'NeoSolarized'
-    end,
-}
